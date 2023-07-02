e750cba4576a4094a2c04afc1f3f9422f4071f89
Pedro Martins <pedromartinsbda@gmail.com>
2023-07-02 19:52:38 -0300

    new commit

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-
   // pegar a tag img
   const img = document.querySelector("#profile img")
-
   // substituir a imagem
   if (html.classList.contains('light')) {
     // se tiver light mode, adiocionar a imagem light
